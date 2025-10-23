57efefad2598470ff1a213400ed60586caa2a11b
ayuubthegreat <yusufayuub00@gmail.com>
2025-10-23 08:28:14 -0500

    git commit -m "Added button and special-section styles to index.css"

--- src/pages/mainHub.jsx
+++ src/pages/mainHub.jsx
@@ -2,13 +2,22 @@ import { Link } from "react-router-dom";
 
 
 
+
 function MainHub() {
     return(
         <div className="flex flex-col items-center h-screen bg-yellow-100">
-            <div className="text-center border-2 border-red-300 p-10 rounded-lg shadow-lg bg-white w-full h-1/2 flex flex-col justify-center">
+            <div className="special-section text-center">
             <h1 className="text-4xl">Welcome to ZenJournal</h1>
-            <p className="text-lg">Your personal space to relax and reflect.</p>
-            <Link to="/signup" className="button mt-4 self-center">Get Started</Link>
+            <p className="text-lg mt-3">Your personal space to relax and reflect.</p>
+            <input type="text" placeholder="What shall you write about?" className="mt-4 p-3 rounded-md w-1/2 self-center"/>
+            </div>
+            <div className="special-section">
+            <h1 className="text-4xl">A trusted, safe space</h1>
+            <p className="text-lg mt-3">Your personal space to relax and reflect.</p>
+            <div className="flex justify-start">
+                <Link to="/signup" className="button mt-4">Get Started</Link>
+                <Link to="/login" className="button mt-4 ml-4">Login</Link>
+            </div>
             </div>
             
         </div>
